237e309b152d6f03ca2428f46922532194a718b7
Atanas Minev <atmin@pan0.com>
2020-09-06 09:18:49 +0300

    fork out cloudflare worker

---
+++ README.md
@@ -0,0 +1,19 @@
+# pan0.net
+
+## What
+
+`pan0.net` is a CDN and a MIT-licensed library for rapid creation of interlinked, interactive 3D scenes.
+
+## Features
+
+- functions available on global CDN (Cloudflare) and as npm package
+- based on ES6 modules, download only what you use
+- powered by BabylonJS
+
+## Examples
+
+- hello-world.html
+- link.html
+- boolean.html
+- material.html
+- interaction.html
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import box from './box';
+import camera from './camera';
+import environment from './environment';
+import ground from './ground';
+import lights from './lights';
+import render from './render';
+import scene from './scene';
+import SceneObject from './SceneObject';
+import sphere from './sphere';
+
+export default {
+  box,
+  camera,
+  environment,
+  ground,
+  lights,
+  render,
+  scene,
+  SceneObject,
+  sphere,
+};
