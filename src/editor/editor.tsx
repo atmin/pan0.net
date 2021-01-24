7127150e344ea0fae3e8625f0105f9992decd78f
Atanas Minev <atmin@pan0.com>
2021-01-24 13:03:09 +0200

    work on editor

--- src/editor/editor.tsx
+++ src/editor/editor.tsx
@@ -1,29 +1,37 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import babel from 'prettier/parser-babel';
 import prettier from 'prettier/standalone';
 import MonacoEditor from '@monaco-editor/react';
 
-const PRETTIER_OPTIONS = {
-  parser: 'babel' as const,
-  plugins: [babel],
-  printWidth: 40,
-  semi: false,
-  trailingComma: 'es5' as const,
-};
-
 export const Editor = () => {
   const editorRef = useRef(null);
+
+  const [source, setSource] = useState(null);
+  const [ast, setAst] = useState(null);
+  const [position, setPosition] = useState(null);
 
   const handleEditorDidMount = useCallback(
     (editor, monaco) => {
       editorRef.current = editor;
 
+      // useful AST explorer: https://astexplorer.net/
       monaco.languages.registerDocumentFormattingEditProvider('javascript', {
         provideDocumentFormattingEdits(model: any) {
           return [
             {
               range: model.getFullModelRange(),
-              text: prettier.format(model.getValue(), PRETTIER_OPTIONS),
+              text: prettier.format(model.getValue(), {
+                parser(text, { babel }) {
+                  const ast = babel(text);
+                  setAst(ast);
+                  console.log(ast);
+                  return ast;
+                },
+                plugins: [babel],
+                printWidth: 40,
+                semi: false,
+                trailingComma: 'es5' as const,
+              }),
             },
           ];
         },
@@ -33,18 +41,24 @@ export const Editor = () => {
         id: 'save',
         label: 'Save',
         keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_S],
-        async run() {
-          await editorRef.current
-            .getAction('editor.action.formatDocument')
-            .run();
+        run() {
+          editorRef.current.getAction('editor.action.formatDocument').run();
 
           localStorage.setItem(location.pathname, editorRef.current.getValue());
         },
       });
 
-      editor.onDidChangeCursorPosition(({ position, reason, source }) => {
-        console.log({ position, reason, source });
+      editor.onDidChangeCursorPosition(({ position }) => {
+        setPosition(position);
       });
+    },
+    [editorRef]
+  );
+
+  const handleEditorChange = useCallback(
+    (value, op) => {
+      setSource(value);
+      console.log(op);
     },
     [editorRef]
   );
@@ -61,6 +75,7 @@ export const Editor = () => {
         // automaticLayout: true,
       }}
       onMount={handleEditorDidMount}
+      onChange={handleEditorChange}
     />
   );
 };
